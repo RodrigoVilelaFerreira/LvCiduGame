adf54dcdac281a9b396d6aacf1b3d7d633a72d4c
Rodrigo Vilela Ferreira <rod.vf@hotmail.com>
2022-06-21 19:00:16 -0300

    moon added and animations fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
 
@@ -12,22 +13,45 @@ const jump = () => {
 const loopGame = setInterval(() => {
   const pipePosition = pipe.offsetLeft;
   const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
-
+  .getComputedStyle(mario)
+  .bottom.replace("px", "");
+  
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
-
+    
     mario.style.animation = "none";
     mario.style.bottom = `${marioPosition}px`;
-
+    
     mario.src = "./Images/lvcidu-game-over-new.png";
     mario.style.width = "150px";
     mario.style.marginLeft = "5px";
-
+    
+    clearInterval(timerVariable);
     clearInterval(loopGame);
   }
 }, 10);
 
 document.addEventListener("keydown", jump);
+
+//TIMER 
+
+let timerVariable = setInterval(countUpTimer, 1000);
+ totalSeconds = 0;
+
+function countUpTimer() {
+  ++totalSeconds;
+  let hour = Math.floor(totalSeconds / 3600);
+  let minute = Math.floor((totalSeconds - hour * 3600) / 60);
+  let seconds = totalSeconds - (hour * 3600 + minute * 60);
+
+  if(hour < 10)
+     hour = "0"+hour;
+  if(minute < 10)
+     minute = "0"+minute;
+  if(seconds < 10)
+     seconds = "0"+seconds;
+  document.getElementById("count_up_timer").innerHTML = `${hour}:${minute}:${seconds}`;
+}
+
+// TIMER END
