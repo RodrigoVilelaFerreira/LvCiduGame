11d3d7e782cc2c3ab72e7993869e106a1990b7a7
Rodrigo Ferreira <rodrigo-ferreira@grupoamp.local>
2022-10-19 10:15:14 -0300

    score implementing

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const mainCharacter = document.querySelector(".mainCharacter");
 const SimpleObstacle1 = document.querySelector(".SimpleObstacle1-game");
 const restartButton = document.querySelector("#restartButton");
+const gameOverScreen = document.querySelector("#gameOverScreen")
 const jumpCounter = document.getElementById("jump_counter");
+const difficultyLevel = document.getElementById("difficulty_level");
+const countUpTime = document.getElementById("count_up_timer")
+const scoreCounter = document.getElementById("score_counter")
 let jumpCount = 0;
 let gameIsRunning = true;
 
@@ -44,7 +48,8 @@ const loopGame = setInterval(() => {
     mainCharacter.src = "./Images/lvcidu-game-over-new.png";
     mainCharacter.style.width = "150px";
     mainCharacter.style.marginLeft = "5px";
-    restartButton.style.display = "block";
+    gameOverScreen.style.display = "block";
+    calculateScore();
 
     gameIsRunning = false;
   
@@ -67,9 +72,7 @@ function countUpTimer() {
   if (hour < 10) hour = "0" + hour;
   if (minute < 10) minute = "0" + minute;
   if (seconds < 10) seconds = "0" + seconds;
-  document.getElementById(
-    "count_up_timer"
-    ).innerHTML = `${hour}:${minute}:${seconds}`;
+  countUpTime.innerHTML = `${hour}:${minute}:${seconds}`;
 }
 
 //Restart Button
@@ -84,6 +87,7 @@ restartButton.onclick = function () {
 let difficultyCounter = 5
 let difficulty = 0
 let SimpleObstacle1_Position0 = false
+let levelCount = 1
 
 SimpleObstacle1.style.animation = `SimpleObstacle1-animation linear ${1.5 - difficulty}s infinite normal none running`
 
@@ -104,19 +108,57 @@ function ChangeDifficulty() {
     difficultyCounter += 5
     difficulty += 0.1
 
+    levelCount += 1;
+    difficultyLevel.innerHTML = `${levelCount}`;
+
+    /*
     console.log(SimpleObstacle1Position)
     console.log(windowWidth)
     console.log((animationDurationNumber * 1000).toFixed(5))
     console.log((animationPixelPerSecond * 1000).toFixed(5));
     console.log((timeEndAnimationMilisec).toFixed(5));
-    
+    */
+
     setTimeout(function waitAnimation() {
       SimpleObstacle1.style.animation = `none`
 
       setTimeout(() => {
         SimpleObstacle1.style.animation = `SimpleObstacle1-animation linear ${1.5 - difficulty}s infinite normal none running`
-        console.log(timeEndAnimationMilisec)
+        //console.log(timeEndAnimationMilisec)
       }, 100);
     }, timeEndAnimationMilisec);
   }
+}
+
+
+//CALCULATE SCORE
+function calculateScore() {
+  let scoreCount = 1
+  let totalJump = jumpCounter.innerHTML
+  let totalTime = countUpTime.innerHTML
+  let difficultyBonus = difficultyLevel.innerHTML
+
+  let seconds = totalTime.slice(-2);
+  let minutes = totalTime.slice(3,5);
+  let hours = totalTime.slice(0,2);
+  let timePoints = (seconds*1000) + (minutes*60000) + (hours*3600000)
+
+  /*
+  console.log(totalJump);
+  console.log(totalTime);
+  console.log(seconds);
+  console.log(minutes);
+  console.log(hours);
+  console.log(timePoints);
+  console.log(difficultyBonus);
+  */
+  
+  let totalPoints = ((totalJump*5000000)/timePoints)
+
+  console.log(totalPoints)
+  console.log(difficultyBonus)
+
+  scoreCount = Math.round(totalPoints*(difficultyBonus/4))
+
+  scoreCounter.innerHTML = `${scoreCount}`;
 }
